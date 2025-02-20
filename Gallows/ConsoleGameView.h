97eba81f31252d6eb8a7f8e537b0f00e0e592fbe
User <asphaltlev@gmail.com>
2025-02-20 21:21:34 +0200

    The output of the man with the gallows has been improved.

--- Gallows/ConsoleGameView.h
+++ Gallows/ConsoleGameView.h
@@ -4,73 +4,143 @@
 
 class ConsoleGameView : public IGameView
 {
-	int mistakes = 0;
+    int mistakes = 0;
 public:
-	void DisplayWord(const std::string& word) override
-	{
-		for (char letter : word)
-		{
-			if (letter == ' ')
-			{
-				std::cout << "_ ";
-			}
-			else {
-				std::cout << letter << ' ';
-			}
-		}
-		std::cout << std::endl;
-	}
+    void DisplayWord(const std::string& word) override
+    {
+        for (char letter : word)
+        {
+            if (letter == ' ')
+            {
+                std::cout << "_ ";
+            }
+            else {
+                std::cout << letter << ' ';
+            }
+        }
+        std::cout << std::endl;
+    }
 
-	void DisplayMistakes(int mistakes) override
-	{
-		std::cout << "Mistakes: (" << mistakes << "/6)" << std::endl;
-	}
+    void DisplayMistakes(int mistakes) override
+    {
+        std::cout << "Mistakes: (" << mistakes << "/6)" << std::endl;
+        DrawHangman(mistakes);
+    }
 
-	virtual void WordGuessed() override
-	{
-		std::cout << "Word Guessed !!!" << std::endl;
-	}
+    void DrawHangman(int mistakes)
+    {
+        switch (mistakes)
+        {
+        case 0:
+            std::cout << "\n";
+            std::cout << "  +---+" << std::endl;
+            std::cout << "  |   |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "=========" << std::endl;
+            break;
+        case 1:
+            std::cout << "  +---+" << std::endl;
+            std::cout << "  |   |" << std::endl;
+            std::cout << "  O   |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "=========" << std::endl;
+            break;
+        case 2:
+            std::cout << "  +---+" << std::endl;
+            std::cout << "  |   |" << std::endl;
+            std::cout << "  O   |" << std::endl;
+            std::cout << "  |   |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "=========" << std::endl;
+            break;
+        case 3:
+            std::cout << "  +---+" << std::endl;
+            std::cout << "  |   |" << std::endl;
+            std::cout << "  O   |" << std::endl;
+            std::cout << " /|   |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "=========" << std::endl;
+            break;
+        case 4:
+            std::cout << "  +---+" << std::endl;
+            std::cout << "  |   |" << std::endl;
+            std::cout << "  O   |" << std::endl;
+            std::cout << " /|\\  |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "=========" << std::endl;
+            break;
+        case 5:
+            std::cout << "  +---+" << std::endl;
+            std::cout << "  |   |" << std::endl;
+            std::cout << "  O   |" << std::endl;
+            std::cout << " /|\\  |" << std::endl;
+            std::cout << " /    |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "=========" << std::endl;
+            break;
+        case 6:
+            std::cout << "  +---+" << std::endl;
+            std::cout << "  |   |" << std::endl;
+            std::cout << "  O   |" << std::endl;
+            std::cout << " /|\\  |" << std::endl;
+            std::cout << " / \\  |" << std::endl;
+            std::cout << "      |" << std::endl;
+            std::cout << "=========" << std::endl;
+            break;
+        }
+    }
 
-	virtual void GameOver() override
-	{
-		std::cout << "!!! GameOver !!!\n";
-		system("pause");
-	}
+    virtual void WordGuessed() override
+    {
+        std::cout << "Word Guessed !!!" << std::endl;
+    }
 
-	virtual bool IsGameContinue() override
-	{
-		char input;
-		while (true)
-		{
-			std::cout << "Do you want continue? (y/n): ";
-			std::cin >> input;
-			switch (input)
-			{
-			case 'y':
-			case 'Y':
-				return true;
-				break;
-			case 'n':
-			case 'N':
-				return false;
-				break;
-			default:
-				system("cls");
-				break;
-			}
-		}
-	}
+    virtual void GameOver() override
+    {
+        std::cout << "!!! GameOver !!!\n";
+        system("pause");
+    }
 
-	void DisplayMessage(const std::string& message) override
-	{
-		std::cout << "DisplayMessage: " << message << std::endl;
-	}
+    virtual bool IsGameContinue() override
+    {
+        char input;
+        while (true)
+        {
+            std::cout << "Do you want continue? (y/n): ";
+            std::cin >> input;
+            switch (input)
+            {
+            case 'y':
+            case 'Y':
+                return true;
+            case 'n':
+            case 'N':
+                return false;
+            default:
+                system("cls");
+                break;
+            }
+        }
+    }
 
-	char GetInputLetter() override
-	{
-		char input;
-		std::cout << "Input the letter: ";
-		std::cin >> input;
-		return input;
-	}
+    void DisplayMessage(const std::string& message) override
+    {
+        std::cout << "DisplayMessage: " << message << std::endl;
+    }
+
+    char GetInputLetter() override
+    {
+        char input;
+        std::cout << "Input the letter: ";
+        std::cin >> input;
+        return input;
+    }
 };
